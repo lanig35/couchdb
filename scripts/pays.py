404792af2e2b15c2fb0a16596f5d03e2b258f54a
lanig35 <blanchard.lanig@gmail.com>
2019-11-11 17:38:52 +0100

    création base pays

---
+++ scripts/pays.py
@@ -0,0 +1,34 @@
+#! /usr/bin/env python
+# coding: utf-8
+
+import json
+import requests
+import time
+
+# récupération liste des pays
+with open ('../countries.json', mode='r', encoding='utf8') as f:
+    liste_pays = json.load(f)
+
+# ajout des pays dans la base
+for item in liste_pays.keys ():
+    print (item.lower())
+    r = requests.get ('https://restcountries.eu/rest/v2/alpha/'+item.lower())
+    pays = r.json()
+
+    # récupération drapeau
+    r = requests.get (pays['flag'])
+    headers = {'Content-type': r.headers.get('content-type')}
+    flag = open ('flag.svg', 'wb')
+    flag.write (r.content)
+    flag.close ()
+
+    # ajout du pays
+    r = requests.put ('http://127.0.0.1:5984/pays/'+item.lower(), data=json.dumps (pays))
+    rev = r.json()['rev']
+    print (rev)
+
+    # ajout du drapeau en attachement
+    param = {'rev': rev}
+    r = requests.put ('http://127.0.0.1:5984/pays/'+item.lower()+'/file', params=param, headers=headers, data= open('flag.svg','rb'))
+    print (r.text)
+    time.sleep (1)
